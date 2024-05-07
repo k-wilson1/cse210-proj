0bd88b4aa362cb4cfd67d66776cc7e072602b9bb
k-wilson1 <katsouttathebag@outlook.com>
2024-05-07 14:05:54 -0500

    Week 3 learning

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Fraction f1 = new Fraction();
+        // Console.WriteLine(f1.GetFractionString());
+
+        // Fraction f2 = new Fraction(6);
+        // Console.WriteLine(f2.GetFractionString());
+
+        // Fraction f3 = new Fraction(6,7);
+        // Console.WriteLine(f3.GetFractionString());
+
+        // Fraction f = new Fraction();
+        // f.SetTop(5);
+        // Console.WriteLine(f.GetFractionString());
+
+        // f.SetBottom(10);
+        // Console.WriteLine(f.GetFractionString());
+
+        // f.SetTop(8);
+        // f.SetBottom(3);
+        // Console.WriteLine(f.GetFractionString());
+
+        Fraction f5 = new Fraction(1,3);
+        Console.WriteLine(f5.GetFractionString());
+        Console.WriteLine(f5.GetDecimalValue());
     }
 }
