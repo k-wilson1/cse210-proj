a7959c62086161f2849d773b3ec6684690a2090d
k-wilson1 <katsouttathebag@outlook.com>
2024-05-21 15:49:32 -0500

    Week 5 learning

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square= new Square("White", 3);
+        Rectangle rectangle = new Rectangle("Purple", 4, 9);
+        Circle circle = new Circle("Teal", 6);
+
+        List<Shape> shapes= new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"Shape:{shape} Color:{color} Area:{area}");
+        }
+        
     }
 }
